f6ff9e3a643ccaaf807bcb048031c57ff1684710
Quan Ngo <ngotrungquan1412@gmail.com>
2023-10-24 11:50:11 +0700

    Test2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Product from './Pages/Product/Product';
-import ProductInfo from './Pages/ProductInfo/ProductInfo';
+
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
